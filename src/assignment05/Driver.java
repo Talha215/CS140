398e2fe763e207dbe9226b7a0f9c34f8ab0fa554
Talha215 <bmw215t@aim.com>
2016-03-11 16:13:08 -0500

    added assignment05 Finished Problem 1

---
+++ src/assignment05/Driver.java
@@ -0,0 +1,14 @@
+package assignment05;
+
+public class Driver {
+	
+	public static void main(String[] args) {
+		Parent p = new Parent();
+		p.setpName("Tom");
+		p.print();
+		p = new Child(); 
+		p.setpName("Dick"); 
+		((Child)p).setcName("Harry"); 
+		p.print(); 
+	}
+}
