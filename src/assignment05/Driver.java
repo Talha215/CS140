16bb95010380027499e0e9a641f2132bea0c77c3
Talha215 <bmw215t@aim.com>
2016-03-11 17:02:33 -0500

    finished Problem 3

--- src/assignment05/Driver.java
+++ src/assignment05/Driver.java
@@ -1,5 +1,8 @@
 package assignment05;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Driver {
 	public static void main(String[] args) {
 		Pizza order = new MediumCheesePizza();
@@ -12,5 +15,28 @@ public class Driver {
 		System.out.println(order.getDescription() + " " + order.getCost());
 		order = new Sausage(order);
 		System.out.println(order.getDescription() + " " + order.getCost());
+		
+		System.out.println("\n---Alphabetized---");
+		order = sortToppings(order);
+		System.out.println(order.getDescription() + " " + order.getCost());
+	}
+	
+	public static Pizza sortToppings(Pizza p) {
+		ArrayList<Pizza> list = new ArrayList<Pizza>();
+		
+		while(p instanceof PizzaTopping) {
+			list.add(p);
+			p = ((PizzaTopping)p).getOn();
+		}
+		list.add(p);
+		Collections.sort(list);
+		p = list.get(list.size() - 1);
+		
+		for(int i = list.size() - 2; i >= 0; i--) {
+			((PizzaTopping)list.get(i)).setOn(p); 
+			p = list.get(i);
+		}
+	
+		return p;
 	}
 }
