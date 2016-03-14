008cd2b2e80f111cb2bc1bbe5f085d61402a293c
Talha215 <bmw215t@aim.com>
2016-03-14 11:42:37 -0400

    almost done w/ assignment06 started almost finished question 3

---
+++ src/assignment06/CSAudit.java
@@ -0,0 +1,15 @@
+package assignment06;
+
+import java.util.ArrayList;
+
+public class CSAudit implements DegreeAudit {
+	@Override
+	public boolean hasWritingRequirement() {
+		return true;
+	}
+
+	@Override
+	public boolean writingSatisfied(ArrayList<CourseOffering> list) {
+		return true;
+	}
+}
