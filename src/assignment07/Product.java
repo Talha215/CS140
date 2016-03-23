ce93b2540fb78e30887a827deb7d179146817852
Talha Qureshi <Talha215@users.noreply.github.com>
2016-03-23 19:17:49 -0400

    Update Product.java

--- src/assignment07/Product.java
+++ src/assignment07/Product.java
@@ -4,14 +4,33 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+/**
+ * This class is the framework for the assignment.
+ * It is abstract, with a couple of abstract methods.
+ * It implements Iterable and Comparable, which will be used by its subclasses to
+ * define the hierarchy and compare objects.
+ * 
+ * @author Talha
+ * 
+ * String description : A string used as an identifier to objects
+ * int indent : Used with print later on; also helps organize where it belongs in the hierarchy of objects
+ */
 public abstract class Product implements Iterable<Product>, Comparable<Product> {
 	private String description;
 	private int indent;
 	
+	/**
+	 * This is a default 1-arg constructor. It sets the description field.
+	 * @param description sets the private String description. It's basically the "name" of the Product
+	 */
 	public Product(String description) {
 		this.description = description;
 	}
 	
+	/**
+	 * This iterator is necessary when traversing arrays of Products which contain more products in each element
+	 * For our purposes, it is used in "Subassembly"
+	 */
 	public Iterator<Product> iterator() {
 		ArrayList<Product> list = new ArrayList<Product>();
 		list.add(this);
@@ -19,30 +38,67 @@ public abstract class Product implements Iterable<Product>, Comparable<Product> 
 		return list.iterator();
 	}
 	
+	/**
+	 * This is abstract, so all subclasses must implement their own version.
+	 * @return cost variable or some accumulation of cost. To be implemented in subclasses
+	 */
 	public abstract double getCost();
 	
+	/**
+	 * This is abstract, so all subclasses must implement their own version.
+	 * @return manufatcureTime variable or some accumulation of cost. To be implemented in subclasses
+	 */
 	public abstract double getManufactureTime();
 	
+	/**
+	 * This is abstract, so all subclasses must implement their own version.
+	 * This method adds all of the parts under your object with the correct indentation.
+	 * @param indent the level of layers this object is in the hierarchy.
+	 * @param list the list that involves all of the objects in the hierarchy.
+	 */
 	protected abstract void addAllParts(int indent, List<Product> list);
 	
+	/**
+	 * Will set cost when a subclass has that variable.
+	 * This class does not, therefore, it throws an exception.
+	 * @param cost no use in this method, to be overriden.
+	 */
 	public void setCost(double cost) {
 		throw new UnsupportedOperationException("Method setCost cannot be "
 				+ "called on this object");
 	}
 	
+	/**
+	 * Will set manufatureTime when a subclass has that variable.
+	 * This class does not, therefore, it throws an exception.
+	 * @param manufactureTime no use in this method, to be overriden.
+	 */
 	public void setManufactureTime(double manufactureTime) {
 		throw new UnsupportedOperationException("Method setManufactureTime cannot be "
 				+ "called on this object");
 	}
 
+	/**
+	 * Simple getter method for private indent variable.
+	 * @return int indent variable
+	 */
 	public int getIndent() {
 		return indent;
 	}
 
+	/**
+	 * Simple setter method for private indent variable.
+	 * @param indent the int to set the indent variable to in this instance
+	 */
 	public void setIndent(int indent) {
 		this.indent = indent;
 	}
 	
+	/**
+	 * Method makes and returns a String that is digestible to the user or tester.
+	 * Overrides the generic toString() method that returns an Object's reference.
+	 * Adds "--" the number of times int indent is. This makes it easier to see the hierarchy of objects.
+	 */
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
@@ -54,6 +110,10 @@ public abstract class Product implements Iterable<Product>, Comparable<Product> 
 		return sb.toString();
 	}
 	
+	/**
+	 * This is abstract, so all subclasses must implement their own version.
+	 * @param v
+	 */
 	abstract void accept(Visitor v);
 	
 	public int compareTo(Product other) {
